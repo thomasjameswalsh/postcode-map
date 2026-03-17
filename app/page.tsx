53045acfb784428cac25718d0b97b3788933cc1b
thomas <walsh.jthomas@outlook.com>
2026-03-17 19:41:32 +0000

    Display map; add and remove postcodes.

---
+++ app/page.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { useState } from "react";
+import dynamic from "next/dynamic";
+
+const PostcodeMap = dynamic(
+  () => import('@/components/PostcodeMap'),
+  { ssr: false }
+);
+
+export default function HomePage() {
+  const [input, setInput] = useState("");
+  const [postcodes, setPostcodes] = useState<string[]>([]);
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    const trimmed = input.trim();
+
+    if (!trimmed) {
+      return;
+    }
+
+    setPostcodes([...postcodes, trimmed]);
+    setInput("");
+  }
+
+  return (
+    <div className = "p-6">
+    <h1 className = "text-2xl font-bold">Postcode Map</h1>
+    <div className = "mt-6">
+      <PostcodeMap></PostcodeMap>
+    </div>
+
+    <p className = "mt-2">Select postcodes:</p>
+
+    <form onSubmit = {handleSubmit} className = "mt-4">
+      <input 
+        value = {input}
+        onChange = {(e) => setInput(e.target.value)}
+        placeholder = "e.g. CM21"
+        className = "mt-4 w-full border p-2"
+      />
+
+      <button 
+        type = "submit"
+        className = "mt-2 border px-4 py-2"
+      >Add</button>
+    </form>
+
+    <div className = "mt-6">
+      <h2 className = "font-semibold">Entered Postcode</h2>
+
+      {postcodes.map((postcode, index) => (
+        <div
+          key = {postcode}
+          className = "flex items-center gap-2 rounded-md border px-3 py-1 w-fit">
+        
+          <span>{postcode}</span>
+
+          <button
+            type="button"
+            onClick={() => {
+              const updated = postcodes.filter((p) => p !== postcode);
+              setPostcodes(updated);
+            }}
+            className="flex h-5 w-5 items-center justify-center rounded-full bg-gray-300 text-xs hover:bg-red-500 hover:text-white transition"
+          >✕</button>
+        </div>
+      ))}
+      
+    </div>
+    </div>
+  );
+}
---
+++ components/PostcodeMap.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import postcodePolygons from '@/data/PostcodeDistrictsPolygons_multi.json';
+
+export default function() {
+    const position = [51.505, -0.09];
+
+    type featureCollection = {
+        type: string;
+        features: any[];
+    };
+    const data = postcodePolygons as featureCollection;
+
+    const sampleFeature = data.features.find(
+        (feature: any) => feature.properties.name === "CM21"
+    );
+
+    return (
+        <div className = "h-[500px] w-full">
+        <MapContainer
+            center = {position}
+            zoom = {12}
+            className = "w-full h-full">
+                <TileLayer 
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
+            
+            <GeoJSON data={sampleFeature as any} />
+        </MapContainer>
+        </div>
+    );
+}
